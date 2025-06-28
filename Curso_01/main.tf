113b310214ada44784f708eba353c925507710cc
Pedro Sampaio <sampaio_pedro@outlook.com>
2025-06-28 12:56:59 -0300

    Começando com Ansible

--- Curso_01/main.tf
+++ Curso_01/main.tf
@@ -18,7 +18,14 @@ resource "aws_instance" "app_serve" {
 	ami = "ami-0d1b5a8c13042c939"
 	instance_type = "t2.micro"
 	key_name = "iac-alura"
+	# Subindo junto da infra um html e um servidor http
+	# user_data = <<-EOF
+	# 				#!/bin/bash
+	# 				cd /home/ubuntu
+	# 				echo "<h1>Ola mundo</h1><h2>Feito com Terraform</h2>" > index.html
+	# 				nohup busybox httpd -f -p 8080 &	
+	# 				EOF
 	tags = {
-		Name = "HelloWorld - Terraform"
+		Name = "Terraform e Ansible para python"
 	}
 }
